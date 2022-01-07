2f2ef975fe138a8bb5d9d3e344271ae34ddcd296
hftata <harryftata@gmail.com>
2022-01-07 21:42:52 +0000

    Had a go at lasso/ridge regression classifier

--- R/AMS_base.R
+++ R/AMS_base.R
@@ -15,3 +15,76 @@ AMS_base <- function(s, b, b_reg=10){
 
   return( sqrt(AMS_sq) )
 }
+
+ams_fromWeights <- function(predicted_outcomes, actual_outcomes, weights){
+
+  # # CHECKS ARGUMENTS - Taken this out for now, too unwieldy
+  # # takes 3 vectors of equal length and computes AMS
+  #
+  # # predicted_outcomes and actual_outcomes must be:
+  # # - boolean   (signal==TRUE, background==FALSE), or
+  # # - numeric   (signal==1,    background==0),     or
+  # # - character (signal=="s",  background=="b"),   or
+  # # - factor    (signal=="s",  background=="b")
+  # #
+  # # NB: types are checked,
+  # #     but whether elements are (TRUE, FALSE, 1, 0, 's', 'b') is NOT checked
+  #
+  # # weights must be numeric
+  #
+  # if(!( length(predicted_outcomes)==length(actual_outcomes)
+  #       &
+  #       length(predicted_outcomes)==length(weights) )){
+  #   stop('Lengths of all arguments must be equal.')
+  # }
+  #
+  # if(!( any(is.logical(predicted_outcomes), is.numeric(predicted_outcomes), is.character(predicted_outcomes), is.factor(predicted_outcomes))
+  #       &
+  #       any(is.logical(actual_outcomes), is.numeric(actual_outcomes), is.character(actual_outcomes), is.factor(actual_outcomes))
+  # )){
+  #   stop('predicted_outcomes and actual_outcomes must be logical, numeric, character or factor.')
+  # }
+  #
+  # if(!is.numeric(weights)){
+  #   stop('weights must be numeric.')
+  # }
+
+  # predictions & outcomes should be binary,
+  # throws error  if more than 2 categories present in either field,
+  # gives warning if less than 2 categories present in either field
+  if(length(unique(predicted_outcomes))>2 | length(unique(actual_outcomes))>2){
+    stop('predicted_outcomes and/or actual_outcomes contain more than two categories')
+  } else if(length(unique(predicted_outcomes))<2 | length(unique(actual_outcomes))<2){
+    warning(paste0('predicted_outcomes and actual_outcomes contain ',
+                   length(unique(predicted_outcomes)),
+                   ' and ',
+                   length(unique(actual_outcomes)),
+                   ' categories respectively.')
+    )
+  }
+
+  # put vectors in tibble
+  df <- tibble('pred'=predicted_outcomes, 'act'=actual_outcomes, 'wt'=weights)
+
+  # only interested in observations predicted to be true
+  if(is.logical(df$pred) | is.numeric(df$pred)){
+    df <- df %>% filter(pred!=0)
+  } else if(is.character(df$pred) | is.factor(df$pred)){
+    df <- df %>% filter(pred=='s')
+  }
+
+  # get sums of weights of true and false positives
+  df <- df %>% group_by(act) %>% summarise(wt = sum(wt))
+
+  # extract s (sum of true positive weights) and b (sum of false positive weights)
+  if(is.logical(df$act) | is.numeric(df$act)){
+    s <- filter(df, act!=0)$wt
+    b <- filter(df, act==0)$wt
+  } else if(is.character(df$act) | is.factor(df$act)){
+    s <- filter(df, act!='s')$wt
+    b <- filter(df, act=='b')$wt
+  }
+
+  ams <- AMS_base(s, b)
+  return(ams)
+}
---
+++ R/lasso_ridge.R
@@ -0,0 +1,194 @@
+# library(tidyverse)
+# library(reshape2)
+# library(ggplot2)
+# library(glmnet)
+# library(ROCR)
+# library(caret)
+# library(cvms)
+# library(tibble)
+# library(pROC)
+# library(checkmate)
+# library(BBmisc)
+# library(testit)
+# library(devtools)
+
+library(tidyverse)
+library(caret)
+
+# ---- load training data ----
+
+load('./data/training.RData')
+train <- training
+rm(training)
+
+df_train <- train[,2:33] #remove eventid
+df_train <- df_train[,-31] #remove weights
+
+#df_train$Label=ifelse(df_train$Label=="s",1,0) #encode "s" and "b" to 1 - 0 (resp.) for logistic regresion
+
+label_factor=as.factor(df_train$Label)
+df_train["Label"]=label_factor #need this as factor for caret
+
+weights <- train$Weight # extract weights
+
+# missing values ----------------------------------------------------------
+
+# # 2. Set missing values (currently -999) to column mean
+# df_train <- df_train %>%
+#   mutate(across( !Label, ~na_if(.,-999) ))
+# colmeans <- df_train %>%
+#   summarise(across( !Label , function(x)mean(x, na.rm=TRUE) ))
+# for(i in 1:30){
+#   df_train[[i]] <- replace_na(df_train[[i]], colmeans[[i]])
+# }
+
+# feature selection -------------------------------------------------------
+
+# # as in kaggle winning entry, try removing azimuthal angle features
+# df_train <- df_train %>% select(!ends_with('phi'))
+
+
+# partition training/validation data --------------------------------------
+
+# set aside 20% of training data for validation
+trainIndex <- createDataPartition(df_train$Label, p = .8,list = FALSE,times = 1)
+Train <- df_train[ trainIndex,]
+Valid  <- df_train[-trainIndex,]
+
+# reweight training and validation sets
+N_s <- sum(weights[df_train$Label == 1])
+N_b <- sum(weights[df_train$Label == 0])
+weights_Train <- weights[trainIndex]
+weights_Valid <- weights[-trainIndex]
+reweight <- function(weights, labels, N_s, N_b){
+  new_weights <- weights
+  new_weights[labels == 1] <- weights[labels == 1] * N_s / sum(weights[labels == 1])
+  new_weights[labels == 0] <- weights[labels == 0] * N_b / sum(weights[labels == 0])
+  return(new_weights)
+}
+weights_Train <- reweight(weights_Train, Train$Label, N_s, N_b)
+weights_Valid <- reweight(weights_Valid, Valid$Label, N_s, N_b)
+
+# Try and find a model! ---------------------------------------------------
+
+# for speed while we're tweaking things
+nrows <- 50000
+Train <- slice_head(Train, n=nrows)
+weights_Train <- weights_Train[1:nrows]
+
+# Strategy 1: train without changing threshold ----------------------------
+#             model trained without AMS,
+#             then threshold chosen to maximise AMS on validation set
+
+# train model:
+train_control1 <- trainControl(method = 'cv',
+                              number = 3,
+                              summaryFunction = twoClassSummary,
+                              classProbs = TRUE,
+                              savePredictions = TRUE,
+                              returnData = TRUE)
+
+caret_glmnet1 <- caret::train(Label ~ .,
+                              Train,
+                              trControl = train_control1,
+                              metric = 'Spec',
+                              maximise = TRUE,
+                              method = "glmnet",
+                              weights = weights_Train
+)
+
+sum(caret_glmnet1$pred$obs == 's')
+sum(caret_glmnet1$pred$pred == 's')
+
+# Almost no 's' are predicted during cv
+# (if ~30000 actual 's' observed, only 5-10 's' predicted)
+# playing with summaryFunction/metric doesn't seem to help
+
+# Haven't tried tuning theta (decision rule threshold) with AMS Valid set yet
+
+# Strategy 2: train with AMS ---------------------------------------------------
+
+# can implement manual loss function (summaryFunction argument to trainControl)
+# so can use AMS directly as cv training loss
+
+# make loss function ams for caret trainControl
+ams_summaryFn <- function(data, lev=NULL, model=NULL){
+  ams <- ams_fromWeights(data$pred, data$obs, data$weights)
+  names(ams) <- 'AMS'
+  return(ams)
+}
+
+train_control2 <- trainControl(method = 'cv',
+                              number = 3,
+                              classProbs = TRUE,
+                              savePredictions = TRUE,
+                              returnData = TRUE,
+                              summaryFunction = ams_summaryFn)
+
+caret_glmnet2 <- caret::train(Label ~ .,
+                              Train,
+                              trControl = train_control2,
+                              maximise = TRUE,
+                              method = "glmnet",
+                              metric = 'AMS',
+                              weights = weights_Train
+)
+
+# Fails on such unbalanced data!
+
+# # Works fine if we pick 's' and 'b' with 50/50 probability instead:
+# Train$Label <- factor(sample(c('s','b'), 10000, replace=TRUE), levels=c('s','b'))
+# caret::train(Label ~ .,
+#              Train,
+#              trControl = train_control2,
+#              maximise = TRUE,
+#              method = "glmnet",
+#              metric = 'AMS',
+#              weights = weights_Train
+
+
+# Strategy 2.5: Train with AMS, fudged ------------------------------------
+
+# Try altering the loss function so it calculates a kind of proxy AMS
+# based on class probability (same idea as altering threshold theta)
+
+ams_summaryFn_classProbCutoff <- function(data, lev=NULL, model=NULL){
+  cutoff <- 0.001
+  hackyPreds <- if_else(data$s > cutoff, 's', 'b') %>% as.factor()
+  print(sum(hackyPreds=='s'))
+  ams <- ams_fromWeights(hackyPreds, data$obs, data$weights)
+  names(ams) <- 'AMS'
+  return(ams)
+}
+
+train_control2.5 <- trainControl(method = 'cv',
+                               number = 3,
+                               classProbs = TRUE,
+                               savePredictions = TRUE,
+                               returnData = TRUE,
+                               summaryFunction = ams_summaryFn_classProbCutoff)
+
+caret_glmnet2.5 <- caret::train(Label ~ .,
+                              Train,
+                              trControl = train_control2.5,
+                              maximise = TRUE,
+                              method = "glmnet",
+                              metric = 'AMS',
+                              weights = weights_Train)
+
+# BIGGER PROBLEM: AMS values no good!
+#                 Values bigger than expected and change with sample size
+#                 (running caret_glmnet2.5 with cutoff 0.001:
+#                   10000 samples gives AMS ~20,
+#                   50000 samples gives AMS ~50)
+#
+# Either function is wrong or weights are wrong
+# (need to reweight for each cv sample maybe?)
+
+
+# Strategy 3: Train using AMS & vary threshold in process -----------------
+
+# I'll have a go at implementing this for our case:
+# https://topepo.github.io/caret/using-your-own-model-in-train.html#illustrative-example-5-optimizing-probability-thresholds-for-class-imbalances
+
+
